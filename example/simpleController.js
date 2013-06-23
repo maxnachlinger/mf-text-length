d0b1b4a37a80f559856b59e1531be46dc61f6668
Max Nachlinger <max.nachlinger@gmail.com>
2013-06-22 22:21:59 -0700

    More example work

--- example/simpleController.js
+++ example/simpleController.js
@@ -1,4 +1,4 @@
-angular.module('mf.controllers').controller('SomeController', function ($scope) {
+angular.module('mf.controllers').controller('SimpleController', function ($scope) {
 	$scope.name0 = 'Test Name';
 	$scope.name1 = 'Test Name which is quite long and no doubt violates our text-length';
 });
