1a4056b7134fac62ac7af0c2b104ef58025adb93
Max Nachlinger <max.nachlinger@gmail.com>
2013-06-23 20:04:57 -0700

    Initial pass at tests

--- test/mfTextLengthSpec.js
+++ test/mfTextLengthSpec.js
@@ -1,27 +1,31 @@
 describe('mf.directives Tests', function () {
 	var elm, scope;
+	var maxLength = 20, warnLength = 10;
 
 	beforeEach(module('mf.directives'));
 
 	beforeEach(inject(function ($rootScope, $compile) {
-		scope = $rootScope;
+		scope = $rootScope.$new();
 		scope.testInputValue = "";
 
 		elm = angular.element('<form name="testForm" novalidate><input type="text" id="testInput" ' +
-			'ng-model="testInputValue" mf-text-length="20" mf-text-warn-at="10" required></form>');
+			'ng-model="testInputValue" mf-text-length="' + maxLength + '" mf-text-warn-at="' + warnLength + '" required></form>');
 
 		$compile(elm)(scope);
 		scope.$digest();
 	}));
 
 	it('Should add indictaor on invalid text length', function () {
-		scope.$apply(function() {
+		scope.$apply(function () {
 			scope.testInputValue = "a test value which is beyond our maximum length";
 		});
 
-		var testInput = elm.find('input');
-		expect(testInput).toHaveClass('active');
-		expect(testInput.text()).toBe(scope.testInputValue);
+		var testForm = elm.find('form');
+		expect(testForm.children.length).toBe(2);
+
+		var indicator = elm.find('span');
+		expect(indicator.text()).toBe(scope.testInputValue.length + ' / ' + maxLength);
+		expect(indicator.hasClass('mf-text-length-over'));
 	});
 
 });
